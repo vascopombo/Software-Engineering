317a71521215aae2b760912fc55e9b5f2f5936ff
Hugo Almeida <hugomiguelfa@gmail.com>
2017-03-19 00:08:42 +0000

    Testes roomReserve

--- hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/RoomReserveMethodTest.java
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/RoomReserveMethodTest.java
@@ -7,14 +7,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type;
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
 
 public class RoomReserveMethodTest {
 	Room room;
+	Hotel hotel;
 
 	@Before
 	public void setUp() {
-		Hotel hotel = new Hotel("XPTO123", "Lisboa");
-		this.room = new Room(hotel, "01", Type.SINGLE);
+		this.hotel = new Hotel("XPTO123", "Lisboa");
+		this.room = new Room(this.hotel, "01", Type.SINGLE);
 	}
 
 	@Test
@@ -26,6 +28,28 @@ public class RoomReserveMethodTest {
 		Assert.assertTrue(booking.getReference().length() > 0);
 		Assert.assertEquals(arrival, booking.getArrival());
 		Assert.assertEquals(departure, booking.getDeparture());
+	}
+	
+	@Test(expected = HotelException.class)
+	public void noRoomAvailable() {
+		
+		LocalDate arrival = new LocalDate(2016, 12, 19);
+		LocalDate departure = new LocalDate(2016, 12, 21);
+	    Hotel.reserveHotel(Type.SINGLE, arrival, departure);
+		
+	    LocalDate arrival1 = new LocalDate(2016, 12, 18);
+		LocalDate departure1 = new LocalDate(2016, 12, 20);
+
+		this.room.reserve(Type.SINGLE, arrival1, departure1);
+}
+	
+	@Test(expected = HotelException.class)
+	public void hasNoRoomType() {
+	    LocalDate arrival = new LocalDate(2016, 12, 18);
+		LocalDate departure = new LocalDate(2016, 12, 20);
+
+		this.room.reserve(Type.DOUBLE, arrival, departure);
+		 
 	}
 
 	@After
