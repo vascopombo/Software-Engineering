84e15e974e2ddf578d333d6cba942fa952d0a0ca
AndreFBM <andre_1540@hotmail.com>
2017-03-28 21:16:33 +0100

    Test for Bank.CancelPayment

---
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankCancelPaymentMethodTest.java
@@ -0,0 +1,67 @@
+package pt.ulisboa.tecnico.softeng.bank.domain;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.softeng.bank.domain.Operation.Type;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+
+public class BankCancelPaymentMethodTest {
+	
+	Bank bank;
+	Client client;
+	Operation op;
+	Account account;
+	
+
+	@Before
+	public void setUp() {
+		this.bank = new Bank("Espirito Falido", "EF01");
+		this.client = new Client(this.bank, "Madalena");
+		this.account = new Account(this.bank, this.client);
+		this.op = new Operation(Type.WITHDRAW, this.account, 400);
+		
+	}
+
+	@Test
+	public void success() {
+		
+
+		String reference = this.op.getReference();
+		String cancel = this.bank.cancelPayment(reference);
+		Operation operation = this.bank.getOperation(cancel);
+
+		Assert.assertEquals("DEPOSIT", operation.getType().toString());
+		Assert.assertEquals(this.op.getValue(), operation.getValue());
+		Assert.assertEquals(this.op.getAccount(), operation.getAccount());
+	}
+	
+	@Test(expected = BankException.class)
+	public void nullReference() {
+		Bank.cancelPayment(null);
+	}
+
+	@Test(expected = BankException.class)
+	public void emptyReference() {
+		Bank.cancelPayment("");
+	}
+
+	@Test(expected = BankException.class)
+	public void blankReference() {
+		Bank.cancelPayment("    ");
+	}
+
+	@Test(expected = BankException.class)
+	public void ReferenceDoNoMatch() {
+		Bank.cancelPayment("SL25");
+	}
+
+	@After
+	public void tearDown() {
+		Bank.banks.clear();
+	}
+
+
+}
