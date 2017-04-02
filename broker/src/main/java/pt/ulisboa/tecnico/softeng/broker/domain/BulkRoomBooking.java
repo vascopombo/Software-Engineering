f4a4fed7c8d8b693e8dee0463e2b83b03801e175
JoaoC0sta <joao.costa1195@gmail.com>
2017-04-02 18:42:39 +0100

    Implementação de BulkRoomBooking.processBooking close #34

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBooking.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBooking.java
@@ -9,20 +9,30 @@ import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
 import pt.ulisboa.tecnico.softeng.hotel.dataobjects.RoomBookingData;
 import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
 
 public class BulkRoomBooking {
+	private final int  MAX_HOTEL_EXCEPTIONS = 3;
+	private final int  MAX_REMOTE_ERRORS = 10;
+	
+	private int numberOfHotelExceptions = 0;
+	private int numberOfRemoteErrors = 0;
 	private final Set<String> references = new HashSet<>();
 	private final int number;
 	private final LocalDate arrival;
 	private final LocalDate departure;
-	private final boolean cancelled = false;
+	private boolean cancelled = false;
 
 	public BulkRoomBooking(int number, LocalDate arrival, LocalDate departure) {
 		this.number = number;
 		this.arrival = arrival;
 		this.departure = departure;
 	}
-
+	
+	public Boolean getCancel(){
+		return this.cancelled;
+	}
+	
 	public Set<String> getReferences() {
 		return this.references;
 	}
@@ -38,30 +48,33 @@ public class BulkRoomBooking {
 	public LocalDate getDeparture() {
 		return this.departure;
 	}
+	
 
 	public void processBooking() {
-		if (this.cancelled) {
+		if (this.cancelled==true) {
 			return;
 		}
 
 		try {
-			this.references.addAll(HotelInterface.bulkBooking(this.number, this.arrival, this.departure));
-			// this.numberOfHotelExceptions = 0;
-			// this.numberOfRemoteErrors = 0;
+			Set<String> referencias = new HashSet<String>();
+			referencias = HotelInterface.bulkBooking(this.number, this.arrival, this.departure);
+			this.references.addAll(referencias);
+			this.numberOfHotelExceptions = 0;
+			this.numberOfRemoteErrors = 0;
 			return;
 		} catch (HotelException he) {
-			// this.numberOfHotelExceptions++;
-			// if (this.numberOfHotelExceptions == MAX_HOTEL_EXCEPTIONS) {
-			// this.cancelled = true;
-			// }
-			// this.numberOfRemoteErrors = 0;
+			this.numberOfHotelExceptions++;
+			if (this.numberOfHotelExceptions == MAX_HOTEL_EXCEPTIONS) {
+			this.cancelled = true;
+			 }
+			this.numberOfRemoteErrors = 0;
 			return;
 		} catch (RemoteAccessException rae) {
-			// this.numberOfRemoteErrors++;
-			// if (this.numberOfRemoteErrors == MAX_REMOTE_ERRORS) {
-			// this.cancelled = true;
-			// }
-			// this.numberOfHotelExceptions = 0;
+			this.numberOfRemoteErrors++;
+			if (this.numberOfRemoteErrors == MAX_REMOTE_ERRORS) {
+			this.cancelled = true;
+			 }
+			this.numberOfHotelExceptions = 0;
 			return;
 		}
 	}
