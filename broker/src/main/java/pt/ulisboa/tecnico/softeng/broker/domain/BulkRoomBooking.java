96cf38d06f44d5c48c18caec0bed376ae207da19
Hugo Almeida <hugomiguelfa@gmail.com>
2017-04-15 18:16:54 +0100

    tormar persistente class bulkbooking Closes #45

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBooking.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBooking.java
@@ -5,12 +5,13 @@ import java.util.Set;
 
 import org.joda.time.LocalDate;
 
+import pt.ist.fenixframework.FenixFramework;
 import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
 import pt.ulisboa.tecnico.softeng.hotel.dataobjects.RoomBookingData;
 import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
 
-public class BulkRoomBooking {
+public class BulkRoomBooking extends BulkRoomBooking_Base{
 	public static final int MAX_HOTEL_EXCEPTIONS = 3;
 	public static final int MAX_REMOTE_ERRORS = 10;
 
@@ -26,6 +27,10 @@ public class BulkRoomBooking {
 		this.number = number;
 		this.arrival = arrival;
 		this.departure = departure;
+	}
+	
+	public void delete() {
+		deleteDomainObject();
 	}
 
 	public Set<String> getReferences() {
