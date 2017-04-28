ddb04cbbf391025c9c0a1acb4feb479e6b5984ac
Hugo Almeida <hugomiguelfa@gmail.com>
2017-04-28 17:30:02 +0100

    criar activityprovidercontroller closes #86

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/local/ActivityInterface.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/local/ActivityInterface.java
@@ -1,5 +1,6 @@
 package pt.ulisboa.tecnico.softeng.activity.services.local;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.joda.time.LocalDate;
@@ -12,9 +13,26 @@ import pt.ulisboa.tecnico.softeng.activity.domain.ActivityOffer;
 import pt.ulisboa.tecnico.softeng.activity.domain.ActivityProvider;
 import pt.ulisboa.tecnico.softeng.activity.domain.Booking;
 import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
+import pt.ulisboa.tecnico.softeng.activity.services.local.dataobjects.ActivityProviderData;
 import pt.ulisboa.tecnico.softeng.activity.services.local.dataobjects.ActivityReservationData;
+import pt.ulisboa.tecnico.softeng.activity.services.local.dataobjects.ActivityProviderData.CopyDepth;
 
 public class ActivityInterface {
+	
+	
+	@Atomic(mode = TxMode.READ)
+	public static List<ActivityProviderData> getProviders() {
+		List<ActivityProviderData> providers = new ArrayList<>();
+		for (ActivityProvider provider : FenixFramework.getDomainRoot().getActivityProviderSet()) {
+			providers.add(new ActivityProviderData(provider, CopyDepth.SHALLOW));
+		}
+		return providers;
+	}
+
+	@Atomic(mode = TxMode.WRITE)
+	public static void createProvider(ActivityProviderData providerData) {
+		new ActivityProvider(providerData.getCode(), providerData.getName());
+	}
 
 	@Atomic(mode = TxMode.WRITE)
 	public static String reserveActivity(LocalDate begin, LocalDate end, int age) {
