772a86d1cc9da3f67a60408ca4004139184e8b26
Hugo Almeida <hugomiguelfa@gmail.com>
2017-04-28 17:13:32 +0100

    Criar activityproviderdata Closes #89

---
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/local/dataobjects/ActivityProviderData.java
@@ -0,0 +1,64 @@
+package pt.ulisboa.tecnico.softeng.activity.services.local.dataobjects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pt.ulisboa.tecnico.softeng.activity.domain.ActivityProvider;
+
+
+public class ActivityProviderData {
+	
+	public static enum CopyDepth {
+		SHALLOW, ACTIVITIES
+	};
+
+	private String name;
+	private String code;
+	//private List<ActivityData> activities = new ArrayList<>();
+	
+	public ActivityProviderData() {
+	}
+
+	public ActivityProviderData(ActivityProvider provider, CopyDepth depth) {
+		this.name = provider.getName();
+		this.code = provider.getCode();
+
+		switch (depth) {
+		case ACTIVITIES:
+//			for (Activity activity : provider.getActivitySet()) {
+//				this.activities.add(new ActivityData(activity));
+//			}
+			break;
+		case SHALLOW:
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+//	public List<ActivityData> getActivities() {
+//		return this.activities;
+//	}
+
+//	public void setActivities(List<ActivityData> activities) {
+//		this.activities = activities;
+//	}
+
+}
